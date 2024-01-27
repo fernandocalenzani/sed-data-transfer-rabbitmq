daae824dac795e7467e70ea388c7e6851aea45cc
fernandocalenzani <fernandocalenzani@gmail.com>
2024-01-27 00:20:09 -0300

    feat: implemented to n clients

--- project/pipe/__main__.py
+++ project/pipe/__main__.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import producer.__main__ as Producer
 import psutil
 from dotenv import load_dotenv
+from tabulate import tabulate
 
 load_dotenv()
 
@@ -35,14 +36,17 @@ def start(args):
 
 if __name__ == "__main__":
     try:
-        start_time = time.time()
-
-        tasks = [(Consumer.start, params), (Producer.start, params)]
-
         pid = psutil.Process()
 
-        cpu_before = pid.cpu_percent(interval=0.1)
-        mem_before = pid.memory_info().rss / 1024 / 1024
+        time_t0 = time.time(),
+        cpu_t0 = pid.cpu_percent(interval=0.1)
+        ram_t0 = pid.memory_info().rss / 1024 / 1024
+        mem__t0 = psutil.virtual_memory().percent
+        disk_t0 = psutil.disk_usage('/').percent
+        swap_t0 = psutil.swap_memory().percent
+        battery_t0 = psutil.sensors_battery().percent if psutil.sensors_battery() else None
+
+        tasks = [(Consumer.start, params), (Producer.start, params)]
 
         with Pool() as pool:
             pool.map(start, tasks)
@@ -55,13 +59,23 @@ if __name__ == "__main__":
         pass
 
     finally:
-        end_time = time.time()
-        execution_time = end_time - start_time
-        cpu_after = pid.cpu_percent(interval=0.1)
-        mem_after = pid.memory_info().rss / 1024 / 1024
+        print(f"\n\n[MIMIR] Statistics:")
 
-        print(f"Uso de CPU antes: {cpu_before}% | Depois: {cpu_after}%")
-        print(
-            f"Uso de Memória antes: {mem_before} MB | Depois: {mem_after} MB")
-        print(
-            f"A função levou {execution_time:.2f} segundos para ser concluída.")
+        time_tn = time.time(),
+        cpu_tn = pid.cpu_percent(interval=0.1)
+        ram_tn = pid.memory_info().rss / 1024 / 1024
+        mem__tn = psutil.virtual_memory().percent
+        disk_tn = psutil.disk_usage('/').percent
+        swap_tn = psutil.swap_memory().percent
+        battery_tn = psutil.sensors_battery().percent if psutil.sensors_battery() else None
+
+        table = [
+            ["Start", "End", f"{(time_tn[0] - time_t0[0]):.2f}s"],
+            ["CPU", f"{cpu_t0}%", f"{cpu_tn}%"],
+            ["RAM (MB)", f"{(ram_t0):.2f} MB", f"{(ram_tn):.2f} MB"],
+            ["Disk (MB)", f"{disk_t0} MB", f"{disk_tn} MB"],
+            ["Swap", f"{swap_t0}%", f"{swap_tn}%"],
+            ["Battery", f"{battery_t0}%", f"{battery_tn}%"],
+        ]
+
+        print(tabulate(table, headers="firstrow", tablefmt="fancy_grid"))
